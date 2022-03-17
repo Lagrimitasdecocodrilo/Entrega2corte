2c2e0aed15ec337c7fc046ad7cc257ef2aa8071b
edicion9 <edicion9@laptop-t0k0sr8m.usabcampus.local>
2022-03-17 09:25:43 -0500

    codigo
    escitura codigo

--- entrega 2 corte/Assets/ControlCharacter.cs
+++ entrega 2 corte/Assets/ControlCharacter.cs
@@ -2,18 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControlCharacter : MonoBehaviour
-{
+public class ControlCharacter : MonoBehaviour { 
     public GameObject CharacterPrincipal;
+    public float velocidad = 20f;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
     // Update is called once per frame
-    void Update() {
-        transform.position += Vector3.forward * Time.deltaTime;
-        
+    void Update(){
+        //transform.position += Vector3.forward * Time.deltaTime;
+        if(Input.GetKey(KeyCode.UpArrow)){
+            Transform.position += Vector3.forward * Time.deltaTime;
+        }
+        //Cuando presione la tecla DOWN el carro avanza hacia atrás o en reversa
+        if (Input.GetKey(KeyCode.DownArrow)){
+            Transform.position -= Vector3.forward * Time.deltaTime;
+        }
+        //cuando presione la tecla RIGHT
+        if (Input.GetKey(KeyCode.RightArrow)){
+            //Transform.position += Vector3.right * Time.deltaTime;
+            transform.eulerAngles += new Vector3(0, velocidad * Time.deltaTime, 0);
+        }
+        //cuando presione la tecla LEFT
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            Transform.position -= Vector3.right * Time.deltaTime;
+        }
+
     }
 }
