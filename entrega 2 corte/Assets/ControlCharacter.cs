86ee73ef218dd2e69d21b335bd3af6f6b4893b08
edicion9 <edicion9@laptop-t0k0sr8m.usabcampus.local>
2022-03-17 09:35:27 -0500

    codgo
    se reescribe codigo

--- entrega 2 corte/Assets/ControlCharacter.cs
+++ entrega 2 corte/Assets/ControlCharacter.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ControlCharacter : MonoBehaviour { 
     public GameObject CharacterPrincipal;
     public float velocidad = 20f;
+    public GameObject bala;
     // Start is called before the first frame update
     void Start(){
         
