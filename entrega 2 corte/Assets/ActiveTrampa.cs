0a16a8c8bd1ca121b1c8ffae97af7e24e376b965
edicion9 <edicion9@laptop-t0k0sr8m.usabcampus.local>
2022-03-17 15:39:28 -0500

    Codigo
    se escribe codigo demlos obstaculos

---
+++ entrega 2 corte/Assets/ActiveTrampa.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveTrampa : MonoBehaviour {
+    public GameObject activarTrampa;
+    public Rigidbody rock;
+
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.tag == "Carro") {
+            rock.GetComponent<Rigidbody>().useGravity = true;
+
+        }
+        void OnCollisionEnter(Collision collision) {
+            Debug.Log("Game Over");
+
+        }
+       
+    }
+
+    
+     
+}
